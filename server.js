d39314196625df2fdd33db5d0304afb85aef357d
Liviu Iancu <liviuiancu@gmx.com>
2017-04-16 15:29:59 +0300

    chat is possible between different  users on different sockets

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
   app = express(),
   server = require('http').createServer(app),
-  io = require('socket.io').listen(server);
+  io = require('socket.io').listen(server),
+  usernames = [];
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
@@ -15,8 +16,36 @@ console.log('Server Running...');
 io.sockets.on('connection', function(socket) {
   console.log('Socket Connected...');
 
+  socket.on('new user', function (data, callback) {
+    if (usernames.indexOf(data) != -1) {
+      callback(false);
+    } else {
+      callback(true);
+      socket.username = data;
+      usernames.push(socket.username);
+      updateUsernames();
+    }
+  });
+
+  // update usernames
+  function updateUsernames() {
+    io.sockets.emit('usernames', usernames);
+  }
+
   // send message
   socket.on('send message', function (data) {
-    io.sockets.emit('new message', {msg: data});
+    io.sockets.emit('new message', {
+      msg: data,
+      user: socket.username 
+    });
+  });
+
+  // disconnect
+  socket.on('disconnect', function (data) {
+    if(!socket.username){
+      return;
+    }
+    usernames.splice(usernames.indexOf(socket.username), 1);
+    updateUsernames();
   });
 });
